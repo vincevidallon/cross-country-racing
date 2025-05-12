32d56175e6aea70cab734459f6e50578ac151d56
vvi29 <vvi29@uclive.ac.nz>
2025-05-12 12:47:21 +1200

    Extra work on functionality for shop menu, plus some redesigning of the menu to ensure consistency

--- src/main/java/seng201/team005/gui/MenuShopController.java
+++ src/main/java/seng201/team005/gui/MenuShopController.java
@@ -2,6 +2,7 @@ package seng201.team005.gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
 import seng201.team005.GameEnvironment;
 import javafx.scene.control.Button;
 
@@ -13,14 +14,14 @@ public class MenuShopController extends ScreenController {
     private Button upgradeButton1, upgradeButton2, upgradeButton3, upgradeButton4, upgradeButton5;
 
     @FXML
-    private Button backButton, purchaseCarsButton;
+    private Button backButton, purchaseCarsButton, buyButton;
 
     @FXML
-    private Button selectedPart1, selectedPart2, selectedPart3;
+    private ToggleButton selectedPart1, selectedPart2, selectedPart3;
 
-    private List<Button> upgradeButtons;
-    private List<Button> selectedPartButtons;
-    private int nextSelectedIndex = 0;
+    private List<Button> upgradeButtons = List.of();
+    private List<ToggleButton> selectedParts = List.of();
+    int nextSlotidx = 0;
 
 
     public MenuShopController(GameEnvironment gameEnvironment) {
@@ -37,34 +38,16 @@ public class MenuShopController extends ScreenController {
         return "Shop";
     }
 
-    private void clickBackButton() {
-        getGameEnvironment().launchScreen(new MenuMainController(getGameEnvironment()));
-    }
-
-    private void onUpgradeButtonClicked(ActionEvent event) {
-        if (nextSelectedIndex >= selectedPartButtons.size()) {
-            return;
-        }
-        Button clicked = (Button) event.getSource();
-        String partName = clicked.getText();
-        Button slot = selectedPartButtons.get(nextSelectedIndex);
-        slot.setText(partName);
-        nextSelectedIndex++;
-    }
-
 
     @FXML
     public void initialize() {
         updatePlayerMoneyText();
 
-        upgradeButtons = List.of(upgradeButton1, upgradeButton2, upgradeButton3,
-                                upgradeButton4, upgradeButton5);
+        upgradeButtons = List.of(upgradeButton1);
+        selectedParts = List.of(selectedPart1, selectedPart2, selectedPart3);
+        nextSlotidx = 0;
 
-        selectedPartButtons = List.of(selectedPart1, selectedPart2, selectedPart3);
 
-        for (Button button: upgradeButtons) {
-            button.setOnAction(this::onUpgradeButtonClicked);
-        }
         backButton.setOnAction(event -> getGameEnvironment().launchScreen(new MenuMainController(getGameEnvironment())));
     }
 
