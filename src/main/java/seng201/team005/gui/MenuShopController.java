3f6cd802a4a17e76f1237627e6467449caa12f47
vvi29 <vvi29@uclive.ac.nz>
2025-05-12 19:20:26 +1200

    Mostly finished shop screen. Implemented the toggle functionality to switch between purchasing cars and parts. Will work on getting the stats display to work

--- src/main/java/seng201/team005/gui/MenuShopController.java
+++ src/main/java/seng201/team005/gui/MenuShopController.java
@@ -2,30 +2,44 @@ package seng201.team005.gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
 import seng201.team005.GameEnvironment;
 import javafx.scene.control.Button;
+import seng201.team005.models.Part;
+import seng201.team005.models.Car;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
 
 public class MenuShopController extends ScreenController {
+    @FXML
+    private Label availableLabel;
 
     @FXML
-    private Button upgradeButton1, upgradeButton2, upgradeButton3, upgradeButton4, upgradeButton5;
+    private Label statsLabel;
+
+    @FXML
+    private Button itemButton1, itemButton2, itemButton3, itemButton4, itemButton5;
 
     @FXML
     private Button backButton, purchaseCarsButton, buyButton;
 
     @FXML
-    private ToggleButton selectedPart1, selectedPart2, selectedPart3;
+    private ToggleButton selectedItem1, selectedItem2, selectedItem3;
 
-    private List<Button> upgradeButtons = List.of();
-    private List<ToggleButton> selectedParts = List.of();
-    int nextSlotidx = 0;
+    private List<Button> itemButtons = List.of();
+    private List<ToggleButton> selectedItems = List.of();
+    private int nextSlotidx;
+    private final Random random = new Random();
+    private boolean showCars = false;
 
 
     public MenuShopController(GameEnvironment gameEnvironment) {
-        super (gameEnvironment);
+        super(gameEnvironment);
     }
 
     @Override
@@ -38,32 +52,101 @@ public class MenuShopController extends ScreenController {
         return "Shop";
     }
 
+    private void clearItems() {
+        selectedItems.forEach(item -> {
+            item.setSelected(false);
+            item.setText("");
+            item.setUserData(null);
+        });
+        nextSlotidx = 0;
+    }
+
     private void onUpgradeButtonClicked(ActionEvent event) {
-        if (nextSlotidx >= selectedParts.size()) {
+        if (nextSlotidx >= selectedItems.size()) {
             return;
         }
 
         Button clicked = (Button) event.getSource();
-        ToggleButton slot = selectedParts.get(nextSlotidx);
+        ToggleButton slot = selectedItems.get(nextSlotidx);
         slot.setText(clicked.getText());
         slot.setSelected(true);
         nextSlotidx++;
     }
+
+
+    private void loadParts() {
+        clearItems();
+        availableLabel.setText("Available Parts:");
+        List<Part> parts = new ArrayList<>();
+        for (int i = 0; i < itemButtons.size(); i++) {
+            parts.add(new Part());
+        }
+        Collections.shuffle(parts, random);
+        for (int i = 0; i < itemButtons.size(); i++) {
+            Button button = itemButtons.get(i);
+            Part part = parts.get(i);
+            button.setText(part.getName());
+            button.setUserData(part);
+        }
+    }
+
+
+    private void loadCars() {
+        clearItems();
+        availableLabel.setText("Available cars:");
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < itemButtons.size(); i++) {
+            cars.add(new Car());
+        }
+        Collections.shuffle(cars, random);
+        for (int i = 0; i < itemButtons.size(); i++) {
+            Button button = itemButtons.get(i);
+            Car car = cars.get(i);
+            button.setText(car.getName());
+            button.setUserData(car);
+        }
+    }
+
+
     @FXML
     public void initialize() {
         updatePlayerMoneyText();
 
-        upgradeButtons = List.of(upgradeButton1, upgradeButton2, upgradeButton3, upgradeButton4, upgradeButton5);
-        selectedParts = List.of(selectedPart1, selectedPart2, selectedPart3);
+        itemButtons = List.of(itemButton1, itemButton2, itemButton3, itemButton4, itemButton5);
+        selectedItems = List.of(selectedItem1, selectedItem2, selectedItem3);
         nextSlotidx = 0;
 
-        for (Button button : upgradeButtons) {
-            button.setOnAction(this::onUpgradeButtonClicked);
+        itemButtons.forEach(button -> button.setOnAction(this::onUpgradeButtonClicked));
+
+        for (int i = 0; i < selectedItems.size(); i++) {
+            int index = i;
+            ToggleButton toggleButton = selectedItems.get(i);
+            toggleButton.setOnAction(event -> {
+                if (!toggleButton.isSelected()) {
+                    toggleButton.setText("");
+                    toggleButton.setUserData(null);
+                    nextSlotidx = index;
+                }
+            });
+        }
+
+        purchaseCarsButton.setOnAction(event -> {
+            if (!showCars) {
+                loadCars();
+                showCars = true;
+                purchaseCarsButton.setText("Switch to Parts");
+                statsLabel.setText("Car Stats:");
+            }
+            else {
+                loadParts();
+                showCars = false;
+                purchaseCarsButton.setText("Switch to Cars");
+                statsLabel.setText("Part Stats:");
+            }
+        });
+
+        backButton.setOnAction(event -> getGameEnvironment().launchScreen(new MenuMainController(getGameEnvironment())));
         }
 
 
-        backButton.setOnAction(event -> getGameEnvironment().launchScreen(new MenuMainController(getGameEnvironment())));
-    }
-
-    
 }
