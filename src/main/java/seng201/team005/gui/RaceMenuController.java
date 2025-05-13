02210258b2be0bc2e102929234f11c0a4acf1b47
vvi29 <vvi29@uclive.ac.nz>
2025-05-13 23:02:33 +1200

    Set up a controller class for the race menu, began work on the race menu screen

---
+++ src/main/java/seng201/team005/gui/RaceMenuController.java
@@ -0,0 +1,11 @@
+package seng201.team005.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class RaceMenuController {
+
+    @FXML
+    private Button race1Button, race2Button, race3Button;
+    
+}
