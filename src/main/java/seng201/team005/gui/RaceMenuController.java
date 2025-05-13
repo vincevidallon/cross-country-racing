840c17f92d283b70f923ade267bf064c7008a683
vvi29 <vvi29@uclive.ac.nz>
2025-05-13 23:37:55 +1200

    Uncommented the line in MenuMainController for accessing the race menu screen, able to access the race menu from the main menu now

--- src/main/java/seng201/team005/gui/RaceMenuController.java
+++ src/main/java/seng201/team005/gui/RaceMenuController.java
@@ -2,10 +2,31 @@ package seng201.team005.gui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import seng201.team005.GameEnvironment;
 
-public class RaceMenuController {
+public class RaceMenuController extends ScreenController {
 
     @FXML
     private Button race1Button, race2Button, race3Button;
-    
+
+    @FXML
+    private Button exitToMenuButton;
+
+    public RaceMenuController(GameEnvironment gameEnvironment) {
+        super(gameEnvironment);
+    }
+
+    @Override
+    protected String getFxmlFile() {
+        return "/fxml/race_setup.fxml";
+    }
+
+    @Override
+    protected String getTitle() {
+        return "Race Menu";
+    }
+
+    public void initialize() {
+        exitToMenuButton.setOnAction(event -> getGameEnvironment().launchScreen(new MenuMainController(getGameEnvironment())));
+    }
 }
