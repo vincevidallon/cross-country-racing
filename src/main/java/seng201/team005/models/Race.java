96bcac26a1fbb0c7a848b6b04ba545af1e6a1a2d
sha378 <sha378@uclive.ac.nz>
2025-05-15 16:11:24 +1200

    Updated race class

--- src/main/java/seng201/team005/models/Race.java
+++ src/main/java/seng201/team005/models/Race.java
@@ -1,36 +1,39 @@
 package seng201.team005.models;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Race {
-    private int hours;
+    private int maxDuration;
     private int entries;
     private int prizeMoney;
-    private List<Route> routeList = List.of();
+    private List<Route> routeList = new ArrayList<>();
     private Route selectedRoute;
 
     public Race(int difficulty) {
         Random rng = new Random();
         switch (difficulty) {
             case 0: // easy
-                hours = rng.nextInt(5, 10);
+                maxDuration = rng.nextInt(5, 10);
                 entries = rng.nextInt(1, 5);
                 prizeMoney = rng.nextInt(3, 6);
 
                 for (int i = 0; i < rng.nextInt(1, 3); i++) {
                     routeList.add(new Route());
                 }
+                break;
             case 1: // medium
-                hours = rng.nextInt(10, 15);
+                maxDuration = rng.nextInt(10, 15);
                 entries = rng.nextInt(3, 9);
                 prizeMoney = rng.nextInt(6, 10);
 
                 for (int i = 0; i < rng.nextInt(1, 3); i++) {
                     routeList.add(new Route());
                 }
+                break;
             case 2: // hard
-                hours = rng.nextInt(15, 21);
+                maxDuration = rng.nextInt(15, 21);
                 entries = rng.nextInt(5, 13);
                 prizeMoney = rng.nextInt(10, 16);
 
@@ -40,8 +43,19 @@ public class Race {
         }
     }
 
-    public int getHours() {
-        return hours;
+    @Override
+    public String toString() {
+        return "Race{" +
+                "maxDuration=" + maxDuration +
+                ", entries=" + entries +
+                ", prizeMoney=" + prizeMoney +
+                ", routeList=" + routeList +
+                ", selectedRoute=" + selectedRoute +
+                '}';
+    }
+
+    public int getMaxDuration() {
+        return maxDuration;
     }
 
     public int getEntries() {
