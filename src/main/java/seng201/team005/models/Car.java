bc02660ca6c4f0447880cea99496484264715b16
sha378 <sha378@uclive.ac.nz>
2025-05-22 11:54:45 +1200

    Added main menu string for Car

--- src/main/java/seng201/team005/models/Car.java
+++ src/main/java/seng201/team005/models/Car.java
@@ -1,6 +1,8 @@
 package seng201.team005.models;
 
 
+import seng201.team005.services.MenuService;
+
 public class Car extends Purchasable {
 
     public Car(String name) {
@@ -9,6 +11,10 @@ public class Car extends Purchasable {
 
     public Car() {
         this("Car" + rng.nextInt(1, 10) + rng.nextInt(1, 10));
+    }
+
+    public String mainMenuString() {
+        return name + " (" + (overall > 0 ? MenuService.convertStatToStars(overall) : "...") + ")";
     }
 
 }
