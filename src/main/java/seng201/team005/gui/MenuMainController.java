b2dad7437df5cb156f8cf4501132cb3870ea8519
sha378 <sha378@uclive.ac.nz>
2025-05-21 22:33:31 +1200

    Fleshed out main menu screen GUI (yet to be implemented)

--- src/main/java/seng201/team005/gui/MenuMainController.java
+++ src/main/java/seng201/team005/gui/MenuMainController.java
@@ -2,6 +2,7 @@ package seng201.team005.gui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 import seng201.team005.GameEnvironment;
 
 /**
@@ -10,9 +11,12 @@ import seng201.team005.GameEnvironment;
  * @author seng201 teaching team
  */
 public class MenuMainController extends ScreenController {
+    // TODO: Implement new functionality (stats window, results screen)
+    @FXML
+    private Text thanksText;
 
     @FXML
-    private Button raceButton, shopButton, garageButton, quitButton;
+    private Button raceButton, shopButton, garageButton, quitButton, resultsButton;
 
     public MenuMainController(GameEnvironment gameEnvironment) { super(gameEnvironment); }
 
@@ -23,6 +27,16 @@ public class MenuMainController extends ScreenController {
     protected String getTitle() { return "Cross Country Racing"; }
 
     public void initialize() {
+        if (getGameEnvironment().getCurrentSeason() == getGameEnvironment().getSeasonLength()) {
+            raceButton.setVisible(false);
+            shopButton.setVisible(false);
+            garageButton.setVisible(false);
+            quitButton.setVisible(false);
+
+            thanksText.setVisible(true);
+            resultsButton.setVisible(true);
+            resultsButton.setOnAction(event -> getGameEnvironment().launchScreen(new MenuResultsController(getGameEnvironment())));
+        }
         raceButton.setOnAction(event -> getGameEnvironment().launchScreen(new RaceSetupController(getGameEnvironment())));
         shopButton.setOnAction(event -> getGameEnvironment().launchScreen(new MenuShopController(getGameEnvironment())));
         garageButton.setOnAction(event -> getGameEnvironment().launchScreen(new MenuGarageController(getGameEnvironment())));
