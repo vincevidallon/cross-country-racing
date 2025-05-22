343ad5426387cd04682b5f89c79185b01c9a9d1b
sha378 <sha378@uclive.ac.nz>
2025-05-22 15:22:04 +1200

    Added functionality into results GUI

--- src/main/java/seng201/team005/gui/MenuResultsController.java
+++ src/main/java/seng201/team005/gui/MenuResultsController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import seng201.team005.GameEnvironment;
+import seng201.team005.models.Entrant;
 
 /**
  * Controller for the menu_main.fxml window
@@ -13,10 +14,10 @@ import seng201.team005.GameEnvironment;
 public class MenuResultsController extends ScreenController {
 
     @FXML
-    private Text thanksText;
+    private Text nameText, difficultyText, seasonLengthText, racesCompetedInText, averagePlacingText, finalWinningsText;
 
     @FXML
-    private Button raceButton, shopButton, garageButton, quitButton, resultsButton;
+    private Button playAgainButton, quitButton;
 
     public MenuResultsController(GameEnvironment gameEnvironment) { super(gameEnvironment); }
 
@@ -27,6 +28,15 @@ public class MenuResultsController extends ScreenController {
     protected String getTitle() { return "Cross Country Racing | Results"; }
 
     public void initialize() {
+        nameText.setText(getGameEnvironment().getName() + "'s stats:");
+        difficultyText.setText(getGameEnvironment().getDifficulty() == 1 ? "Normal" : "Hard");
+        seasonLengthText.setText(getGameEnvironment().getSeasonLength() + " races");
+        racesCompetedInText.setText(getGameEnvironment().getNumberOfRacesPlayed() + " / " + getGameEnvironment().getSeasonLength());
+        averagePlacingText.setText(getGameEnvironment().getNumberOfRacesPlayed() > 0
+                ? Entrant.positionString(getGameEnvironment().getAverageRaceResult()) : "...");
+        finalWinningsText.setText("$" + getGameEnvironment().getMoney());
 
+        playAgainButton.setOnAction(event -> getGameEnvironment().resetGame());
+        quitButton.setOnAction(event -> onQuitRequested());
     }
 }
